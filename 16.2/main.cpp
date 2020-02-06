7a46c85ffde77c6ddce1ef7ea7b26c15beeab879
楊為守 <WeiShou_Yang@2001-b400-e3d7-a91b-00c4-e518-2e70-16c4.emome-ip6.hinet.net>
2020-02-06 21:29:51 +0800

    Initial Commit

---
+++ 16.2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  16.2
+//
+//  Created by 楊為守 on 2020/2/6.
+//  Copyright © 2020年 Alley_Tseng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
